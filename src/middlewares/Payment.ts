9ea226a668a6ad195400d15f234e5250d1a14021
bibek magar <bibekmagar746@gmail.com>
2026-04-14 16:37:56 +0545

    initite khalti payment api

--- src/middlewares/Payment.ts
+++ src/middlewares/Payment.ts
@@ -11,6 +11,7 @@ class Payment {
         const amount = 100;
         const transaction_uuid =
             EsewaCredentialsHelper.generateUniqueId();
+        console.log("transaction_uuid", transaction_uuid);
         const message = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=${config.ESEWA_MERCHENT_ID}`;
 
         const signature = EsewaCredentialsHelper.generateHash(
@@ -48,11 +49,113 @@ class Payment {
                 data: response.data,
                 transaction_uuid,
             };
+
         } catch (error: any) {
             console.error("eSewa Error:", error?.response?.data || error.message);
-
             throw new Error(
-                error?.response?.data?.message || "Payment initiation failed"
+                error?.response?.data?.message || error.message || "Payment initiation failed"
+            );
+        }
+    }
+
+    public static async verifyEsewaPayment(transaction_uuid: string) {
+        try {
+            const payload = {
+                transaction_uuid,
+                amount: 100,
+                product_code: config.ESEWA_MERCHENT_ID
+            }
+            const response = await axios.get(
+                config.ESEWA_PAYMENT_STATUS_CHECK_URL as string,
+                {
+                    params: payload,
+                }
+            );
+
+            return {
+                success: true,
+                data: response.data,
+            };
+        } catch (error: any) {
+            console.error("eSewa Error:", error?.response?.data || error.message);
+            throw new Error(
+                error?.response?.data?.message || "Payment verification failed"
+            );
+        }
+    }
+
+    public static async inititeKhaltiPayment(data: IPaymentData) {
+        const amount = 100 * 100;
+        const purchase_order_id =
+            EsewaCredentialsHelper.generateUniqueId();
+        const payload = {
+            "amount": amount,
+            "return_url": config.PAYMENT_SUCCESS_URL,
+            "website_url": config.FRONTEND_URL,
+            "purchase_order_id": purchase_order_id,
+            "purchase_order_name": "test",
+            "customer_info": {
+                "name": "Ram Bahadur",
+                "email": "test@khalti.com",
+                "phone": "9800000001"
+            }
+        };
+
+        try {
+            const response = await axios.post(
+                config.KHALTI_PAYMENT_INITIATE_URL as string,
+                payload,
+                {
+                    headers: {
+                        "Authorization": `Key ${config.KHALTI_SECRET_KEY as string}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            console.log("response", response.data);
+
+            return {
+                success: true,
+                pidx: response.data.pidx,
+                payment_url: response.data.payment_url,
+                expires_at: response.data.expires_at,
+                expires_in: response.data.expires_in,
+                purchase_order_id,
+            };
+        } catch (error: any) {
+            console.error("Khalti Error:", error?.response?.data || error.message);
+            throw new Error(
+                error?.response?.data?.message || error.message || "Payment initiation failed"
+            );
+        }
+    }
+
+    public static async verifyKhaltiPayment(pidx: string) {
+        try {
+            const payload = {
+                "pidx": "YZwkAxPGmeiW233quZfFBB"
+            }
+            const response = await axios.post(
+                config.KHALTI_PAYMENT_STATUS_CHECK_URL as string,
+                payload,
+                {
+                    headers: {
+                        "Authorization": `Key ${config.KHALTI_SECRET_KEY as string}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log("response", response.data);
+
+            return {
+                success: true,
+                data: response.data,
+            };
+        } catch (error: any) {
+            console.error("Khalti Error:", error?.response?.data || error.message);
+            throw new Error(
+                error?.response?.data?.message || "Payment verification failed"
             );
         }
     }
--- src/routes/PaymentRoute.ts
+++ src/routes/PaymentRoute.ts
@@ -8,5 +8,21 @@ router
     .post(
         Payment.inititeEsewaPayment);
 
+router
+    .route("/verify-esewa-payment")
+    .post(
+        Payment.verifyEsewaPayment);
+
+router
+    .route("/initite-khalti-payment")
+    .post(
+        Payment.inititeKhaltiPayment);
+
+router
+    .route("/verify-khalti-payment")
+    .post(
+        Payment.verifyKhaltiPayment);
+
+
 
 export default router;
