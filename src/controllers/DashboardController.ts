6011af7667b9c3ea07eeddc202cf1e8fe3070ef6
bibek magar <bibekmagar746@gmail.com>
2026-07-16 21:10:15 +0545

    make dashboard also abvaliable for superadmin

--- src/controllers/DashboardController.ts
+++ src/controllers/DashboardController.ts
@@ -5,6 +5,7 @@ import { AuthRequest } from "../middlewares/Auth";
 class DashboardController {
     static async getDashboardStats(req: AuthRequest, res: Response) {
         const farmId = req.user!.farmId || 0;
+       
         const data = await DashboardService.getDashboardStats(farmId);
 
         return res.status(200).json({
